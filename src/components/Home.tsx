5c360dd3d4f1f7f1d8f5c4d815f0ec51b57aa326
Sanniv Choudhuri <sanniv.1020@gmail.com>
2024-03-19 13:18:00 +0530

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     }
     setLoading(true)
     try{
-      const response = await axios.post("http://15.206.169.115:3000/api/v1/create", {
+      const response = await axios.post("http://15.206.169.115:443/api/v1/create", {
         username: username,
         language: lang,
         stdin: stdin,
